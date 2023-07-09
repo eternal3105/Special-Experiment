e22ba5d6f02c09a40f55e145334a321b64ded602
Yao <101783358+Sanlin1030142@users.noreply.github.com>
2023-07-09 20:47:54 +0800

    Add files via upload
    listen the topic /map and show current map automatically .

---
+++ showmap/map.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import rospy
+from nav_msgs.msg import OccupancyGrid
+import numpy as np
+import matplotlib.pyplot as plt
+
+def map_callback(msg):
+    # encode
+    width = msg.info.width
+    height = msg.info.height
+    data = np.array(msg.data).reshape((height, width))
+
+    # show map
+    plt.imshow(data, cmap='gray')
+    plt.axis('off')
+    plt.show()
+
+rospy.init_node('map_subscriber')
+rospy.Subscriber('/map', OccupancyGrid, map_callback)
+rospy.spin()
